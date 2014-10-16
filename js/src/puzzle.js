69e6d3ba498528676147b586fe68043e400f03ea
Ramiro Ergueta <ramiroerg@gmail.com>
2014-10-16 15:49:22 -0400

    clean function

--- js/src/puzzle.js
+++ js/src/puzzle.js
@@ -1,5 +1,9 @@
 var myPuzzle = function(){
 
+   	/**
+     * Get the size of the matrix for the game
+     * @return {int} the size of the matrix
+     */
 	var matrixSize = function(){
 		do {
 			var n = prompt('Ingrese el tamaño de la tabla que quiere crear (por dejefcto creara una tabla 4x4)');
@@ -13,6 +17,10 @@ var myPuzzle = function(){
 	};
 
 
+	 /**
+     * fill the matrix with random letter, pair letters
+     * @param {int} size of the matrix
+     */
 	var fillMatrix = function(n){
 		var iRow = iCol = letterInt = 0;
 		var myMatrix=new Array(n);
@@ -23,8 +31,7 @@ var myPuzzle = function(){
 		//build the matrix
 		for(i=0; i<n; i++){
 			for(j=0; j<n; j++){
-				myMatrix[i][j] = null;
-				
+				myMatrix[i][j] = null;				
 			}	
 		}
 		
@@ -37,7 +44,6 @@ var myPuzzle = function(){
 					iCol = Math.floor((Math.random() * n)) ;
 
 					if (myMatrix[iRow][iCol] == null){
-						//console.log(index);
 						myMatrix[iRow][iCol] = String.fromCharCode(65+ i);
 						pairLetters++;
 						isCellReady = true;
@@ -49,6 +55,11 @@ var myPuzzle = function(){
 			return myMatrix;
 	};
 
+
+	 /**
+     * Print the matrix in the console
+     * @param {array} matrix to print
+     */
 	var printMatrix = function(m)
 	{
 		var printText = indexHeader = '';
@@ -77,18 +88,23 @@ var myPuzzle = function(){
 
 	};
 
+	 /**
+     * Get coordinates from the user
+     * @param {text} text for the message {int} size of the matrix
+     * @return {in} when user don't enter a value {object} return an object with the coordinates 
+     */
+
 	var  getIndex = function(text,s)
 	{
 		//Get cords and validate
 		do
 		{
-		var input = prompt('Insert los indices del '+text+' valor separados por coma (fila,columna)','1,0');
-
-		if (input == null){
-			return 0;
-		}
+			var input = prompt('Insert los indices del '+text+' valor separados por coma (fila,columna)','1,0');
+			if (input == null){
+				return 0;
+			}
 			
-		var regValidation = new RegExp('^[0-'+s+'],[0-'+s+']$'); 
+			var regValidation = new RegExp('^[0-'+s+'],[0-'+s+']$'); 
 
 		} while(regValidation.test(input)==false);
 
@@ -101,17 +117,12 @@ var myPuzzle = function(){
 
 
 	/*
-
-	Main
-
+	* Main
 	*/
-	var  size = matrixSize();
-
-
-	var matrix = fillMatrix(size);
-
-	var	resultMatrix =new Array(size);
-	var tempMatrix =new Array(size);
+	var size = matrixSize(); //size of the matrix
+	var matrix = fillMatrix(size); //Matrix with the original data to compare with
+	var	resultMatrix =new Array(size); //Results original filled with *
+	var tempMatrix =new Array(size); //Temporal Matrix to shown teporal results
 
 	for (i=0; i <size; i++){
 		resultMatrix[i]=new Array(size)
@@ -121,63 +132,44 @@ var myPuzzle = function(){
 	for(i=0; i<size; i++){
 		for(j=0; j<size; j++){
 			resultMatrix[i][j] = '*';
-			tempMatrix[i][j] = '*';
-				
-			}	
-		}
+			tempMatrix[i][j] = '*';		
+		}	
+	}
 	
-	//Compare matrix
-
-	printMatrix(matrix);
+	// Commenting the matrix with the results
+	//printMatrix(matrix);
 	printMatrix(resultMatrix);
 	var pares = 0;
-
 	do {
-	
-
-
-
+		//Get the first index
 		var firstIndex = getIndex('PRIMER', size);
-
 		if (firstIndex != 0) {
-
 			tempMatrix[firstIndex.row][firstIndex.col] = matrix[firstIndex.row][firstIndex.col];
 			console.clear();
 			printMatrix(tempMatrix);
-
 		}
 
-
+		//Get the second index
 		var secondIndex = getIndex('SEGUNDO', size);
-
 		if (secondIndex != 0) {
-
 			tempMatrix[secondIndex.row][secondIndex.col] = matrix[secondIndex.row][secondIndex.col];
 			console.clear();
 			printMatrix(tempMatrix);
-
 		}
 
-
-
+		//End the game if the user didn't enter both fields
 		if(firstIndex == 0 && secondIndex == 0){
 			console.log('Juego Finalizado');
 			return;
-
 		}
 
-
+		//	update right results
 		if(firstIndex != 0 && secondIndex != 0){
-
 			var sameIndex = false;
-
 			if (firstIndex.row == secondIndex.row && firstIndex.col == secondIndex.col ) {
 			sameIndex = true;
 			};
-
-		
 			if (matrix[firstIndex.row][firstIndex.col]==matrix[secondIndex.row][secondIndex.col] && !sameIndex){
-
 				resultMatrix[firstIndex.row][firstIndex.col] = matrix[firstIndex.row][firstIndex.col];
 				resultMatrix[secondIndex.row][secondIndex.col] = matrix[secondIndex.row][secondIndex.col];
 				pares++;
@@ -185,21 +177,16 @@ var myPuzzle = function(){
 			}
 		}
 
-
+		//Clean temporal matrix
 		for(i=0; i<size; i++){
 			for(j=0; j<size; j++){
-				tempMatrix[i][j] = resultMatrix[i][j];
-			
+				tempMatrix[i][j] = resultMatrix[i][j];		
 			}	
 		}
 
 
-	
-
 	} while(pares < ((size*size)/2));
-
 	console.log('Felicidades, terminaste el juego!');
-
 }
 
 
